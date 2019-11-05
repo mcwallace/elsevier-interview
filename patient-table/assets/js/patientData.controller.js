e24a4ce53b36e97e3e314df183b0d472d96d768f
Cait Wallace <cwallace@connectify.me>
2019-11-04 21:25:08 -0500

    link works, winnowing data

--- patient-table/assets/js/patientData.controller.js
+++ patient-table/assets/js/patientData.controller.js
@@ -9,12 +9,12 @@ patientTable.controller('patientDataController', ['$scope', '$http', function ($
   };
   $scope.badData = true;
   $scope.data = [];
+  $scope.loading = false;
 
   $scope.makeCall = (input) => {
     const patientUrl = `https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Patient/${input}`;
     $http.get(patientUrl).then((patientData) => {
       console.log(patientData);
-      console.log(patientData.data.name);
       const name = patientData.data.name[0];
       $scope.patient.name = name.given[0] + ' ' + name.family[0];
       $scope.patient.gender = patientData.data.gender;
@@ -22,13 +22,17 @@ patientTable.controller('patientDataController', ['$scope', '$http', function ($
       const dob = new Date(patientData.data.birthDate);
       $scope.patient.age = now.getFullYear() - dob.getFullYear();
       const url = `https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Condition/?patient=${input}`;
-      console.log('getting conditions');
+      $scope.badData = false;
+      $scope.loading = true;
       $http.get(url)
-      .then((resp) => {
-        console.log('responseeee');
-        console.log(resp);
-        $scope.data = resp;
+      .then((conditionData) => {
+        console.log(conditionData.data.entry);
+        const rawData = conditionData.data.entry;
+        const realData = rawData.filter(row => row.resource.verificationStatus !== 'entered-in-error');
+        console.log(realData);
+        $scope.data = realData.filter(row => row.resource.clinicalStatus === 'active');
         $scope.badData = false;
+        $scope.loading = false;
       }, (err) => {
         console.log(err);
         console.log(err.status);
@@ -41,7 +45,8 @@ patientTable.controller('patientDataController', ['$scope', '$http', function ($
   };
 
   $scope.createLink = (condition) => {
-    return `https://www.ncbi.nlm.nih.gov/pubmed/?term=${condition}`;
+    const hyphenName = condition.replace(' ', '-');
+    return `https://www.ncbi.nlm.nih.gov/pubmed/?term=${hyphenName}`;
   };
 
 
